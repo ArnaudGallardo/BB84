0c111e3a276dc5a5e12556b7abcb4f4124e56b59
xArchange <candice.bentejac@gmail.com>
2015-05-17 23:05:26 +0200

    Déplacement de la méthode arrayWithDiscards de Benchmark à BytesScheme. Ajout de la méthode detection (en remplacement de la méthode eveDetection), qui est un copié/collé, à peu de choses près, de ce que j'avais fait dans Benchmark pour détecter Eve. Ajout de la méthode (annexe) stillContainsBits qui renvoie true ou false selon si le tableau passé en paramètre (supposé être issu de la fonction arrayWithDiscards) contient encore des mesures de bits ou non.

--- src/BytesScheme.java
+++ src/BytesScheme.java
@@ -4,6 +4,7 @@ public class BytesScheme extends AbstractScheme{
 	
 	private static int FIBER = 3; // Percentage of errors induced by the noise in optic fiber
 	
+	// Given a photon and a filter, returns the bit value (accurate if the filter is the right one, randomly otherwise) of a photon
 	private static byte unpolarize(Photon p, Filter f) {
 		Polarization po = f.readPolarPhoton(p);
 		byte tmp;
@@ -29,6 +30,7 @@ public class BytesScheme extends AbstractScheme{
 		return tmp;
 	}
 	
+	// Creates a byte scheme of a given size filled with bytes that are randomly initialized
 	public BytesScheme(int size) {
 		super(size);
 		bytes = new byte[size];
@@ -37,6 +39,7 @@ public class BytesScheme extends AbstractScheme{
 		}
 	}
 	
+	// Creates a byte scheme given an array of bytes
 	public BytesScheme(byte[] b) {
 		super(b.length);
 		bytes = new byte[b.length];
@@ -45,6 +48,7 @@ public class BytesScheme extends AbstractScheme{
 		}
 	}
 	
+	// Creates of byte scheme given a size, a photon scheme and a filter scheme (reading of a photon p with a filter f)
 	public BytesScheme(int size, PhotonScheme ph, FilterScheme po) {
 		super(size);
 		bytes = new byte[size];
@@ -54,6 +58,7 @@ public class BytesScheme extends AbstractScheme{
 		}
 	}
 
+	// Return the bytes of a byte scheme into an array of bytes
 	public byte[] getBytes() {
 		byte[] tmp = new byte[this.bytes.length];
 		for(int i=0;i<this.bytes.length;i++){
@@ -98,27 +103,92 @@ public class BytesScheme extends AbstractScheme{
 		tmp.append(this.getByte(this.getSize()-1));
 		return tmp.toString();
 	}
-	//A MODIFIER, bs == index donc ...
-	public boolean eveDetected(BytesScheme bs,int[] index,int percentOfKey) {
-		int nbOfVerification = (percentOfKey*index.length)/100;
-		int[] checked = new int[nbOfVerification];
-		if(nbOfVerification<index.length) {
-			for(int i=0;i<nbOfVerification;i++) {
-				int random = (int)(Math.random()*index.length);
-				while(isInArray(random,checked)) {
-					random = (int)(Math.random()*index.length);
-				}
-				checked[i] = random;
-				int tmp = index[random];
-				index[random] = -1; //ATTENTION
-				if(this.getByte(tmp)!=bs.getByte(tmp))
-					return true;
+	
+	
+//	//A MODIFIER, bs == index donc ...
+//	public boolean eveDetected(BytesScheme bs,int[] index,int percentOfKey) {
+//		int nbOfVerification = (percentOfKey*index.length)/100;
+//		int[] checked = new int[nbOfVerification];
+//		if(nbOfVerification<index.length) {
+//			for(int i=0;i<nbOfVerification;i++) {
+//				int random = (int)(Math.random()*index.length);
+//				while(isInArray(random,checked)) {
+//					random = (int)(Math.random()*index.length);
+//				}
+//				checked[i] = random;
+//				int tmp = index[random];
+//				index[random] = -1; //ATTENTION
+//				if(this.getByte(tmp)!=bs.getByte(tmp))
+//					return true;
+//			}
+//			return false;
+//		}
+//		return true;
+//	}
+	
+	
+	// The object this method applies to is Alice's original bit key
+	// Given an array of Bob's bit measurement with the 
+	public boolean detection(int[] arrayWithDiscards, int nbSacrificed)
+	{
+		assert(this.getSize() == arrayWithDiscards.length); // Asserts that the byte scheme and the array are of same length
+		// Otherwise, method inadequate
+		
+		int cpt = 0; // Counter of sacrificed photons (= verifications)
+		boolean detected = false; // Is Eve detected?
+		
+		while(nbSacrificed > 0 && cpt <= nbSacrificed && !detected && stillContainsBits(arrayWithDiscards))
+		{
+			int i = (int) (Math.random() * arrayWithDiscards.length); // Generates a random index in arrayWithDiscards
+			while(arrayWithDiscards[i] == -1) // While -1, that's to say while the randomly generated 
+				// index corresponds to an already discarded bit measurement (during the filter exchange or later)
+			{
+				i = (int) (Math.random() * arrayWithDiscards.length);
 			}
-			return false;
+			
+			if(arrayWithDiscards[i] == this.getByte(i))
+			{
+				cpt++;
+				arrayWithDiscards[i] = -1;
+			}
+			else 
+				detected = true;
 		}
-		return true;
+		return detected;
 	}
 	
+	public static boolean stillContainsBits(int[] arrayWithDiscards)
+	{
+		for(int i = 0; i < arrayWithDiscards.length; i++)
+		{
+			if(arrayWithDiscards[i] != -1)
+				return true;
+		}
+		return false;
+	}
+	
+	// Returns an integer array containing Bob's bit measurements (0 or 1) at the indexes where Bob used the same filter as Alice
+	// Or -1 at the indexes where they didn't use the same filters
+	public  int[] arrayWithDiscards(FilterScheme fs1, FilterScheme fs2)
+		{
+			assert(fs1.getSize() == fs2.getSize());
+			int result[] = new int[fs1.getSize()];
+			for(int i = 0; i < result.length; i++)
+			{
+				if(fs1.getFilter(i).equals(fs2.getFilter(i)))
+				{
+					if(this.getByte(i) == 1)
+						result[i] = 1;
+					else
+						result[i] = 0;
+				}
+				else
+					result[i] = -1;
+			}
+			return result;
+		}
+	
+		
 	public byte[] cleanKeyWithIndex(int[] index) {
 		int nb = 0;
 		for(int i=0;i<index.length;i++) {
