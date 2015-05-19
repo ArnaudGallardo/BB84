945de46eab6a4fe0d11451d196b1ecf271542c2a
xArchange <candice.bentejac@gmail.com>
2015-05-19 23:26:08 +0200

    Ajout de commentaires.

--- src/DebugConnectionListener.java
+++ src/DebugConnectionListener.java
@@ -4,16 +4,19 @@ import jexxus.server.ServerConnection;
 
 public class DebugConnectionListener implements ConnectionListener{
 
+	// Tells that the connection between the server and the clients has been lost
   public void connectionBroken(Connection broken, boolean forced){
-    System.out.println("Connection lost: "+broken);
+    System.out.println("Connection lost: "+ broken);
   }
 
+  // Tells that the client has received a message
   public void receive(byte[] data, Connection from){
-    System.out.println("Received message: "+new String(data));
+    System.out.println("Received message: "+ new String(data));
   }
-
+  
+  // Tells that the client is connected
   public void clientConnected(ServerConnection conn){
-    System.out.println("Client Connected: "+conn.getIP());
+    System.out.println("Client Connected: "+ conn.getIP());
   }
 
 }
